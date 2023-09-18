20b8e99c732e7fe35fee3831af1514597f493f6f
Ryan Griffiths <ryantmgr@gmail.com>
2023-09-18 22:48:43 +1000

    SC-236: Rapid - add postcode lookup, DG request/unit_qty  modifcations and item mapping to module.

--- Model/Carrier/SpojitShipping.php
+++ Model/Carrier/SpojitShipping.php
@@ -152,10 +152,12 @@ class SpojitShipping extends AbstractCarrier implements CarrierInterface
             return false;
         }
 
-        // if we don't have postcode/city/state yet don't try to get quotes
-        if (!$request->getDestPostcode() || !$request->getDestCity() || !$request->getDestRegionCode()) {
+        // if we don't have postcode don't try to get quotes
+        if (!$request->getDestPostcode()) {
             return false;
         }
+
+        $hashCheck = ['postcode' => $request->getDestPostcode()];
 
         /** @var \Magento\Quote\Model\Quote\Item $firstItem */
         $firstItem = reset($items);
@@ -171,7 +173,38 @@ class SpojitShipping extends AbstractCarrier implements CarrierInterface
         $itemsData = [];
 
         foreach ($items as $item) {
-            $itemsData[] = $this->objectToArray($item->getData());
+            $itemsDataArray = $this->objectToArray($item->getData());
+            if ($itemsDataArray['product_type'] == 'configurable') {
+                continue;
+            }
+            $unitQty = array_key_exists('unit_qty', $itemsDataArray) ?  $itemsDataArray['unit_qty']: null;
+            // I don't know how the unit_qty renders but it doesn't seems to be an int
+            if ($unitQty == '0') {
+                $unitQty = null;
+            }
+            $qty = $unitQty ? ($unitQty * $itemsDataArray['qty']) : $itemsDataArray['qty'];
+            $hashCheck['items'][] = ['unit_qty' => $unitQty, 'product' => $itemsDataArray['product_id'], 'qty' => $itemsDataArray['qty']];
+            $itemData = [
+                'ref' => $itemsDataArray['sku'],
+                'amt' => $qty,
+                'desc' => 'CARTON',
+                'wgt' => $itemsDataArray['weight'] * $qty,
+                'youritemdesc' => substr($itemsDataArray['name'], 0, 50),
+                'multiplylwh' => 'Y',
+                'len' => 1,
+                'wdt' => 1,
+                'hgt' => 1,
+                'cube' => ($itemsDataArray['weight'] * $qty * 1 * 1 * 1)/1000000, // the *1 are placeholders for lwh if they are added
+            ];
+
+            if (array_key_exists('is_dangerous', $itemsDataArray) && $itemsDataArray['is_dangerous'] == '1') {
+                $itemData['unnumber'] = '1111';
+                $itemData['class'] = '2.1';
+                $itemData['size'] = $qty;
+                $itemData['um'] = 'KG';
+            }
+
+            $itemsData[] = $itemData;
         }
 
         $data = $this->objectToArray($request->getData());
@@ -179,7 +212,7 @@ class SpojitShipping extends AbstractCarrier implements CarrierInterface
 
         $cart = $this->cartCollectionFactory->create()->addFieldToFilter('quote_id', $quote->getId())->getFirstItem();
 
-        $cartHash = md5(json_encode($data));
+        $cartHash = md5(json_encode($hashCheck));
         $refreshQuotes = false;
 
         if (!$cart->getId()) {
@@ -194,11 +227,31 @@ class SpojitShipping extends AbstractCarrier implements CarrierInterface
         }
 
         if ($refreshQuotes) {
+            $foundData = null;
+            $postcodes = file_get_contents(__DIR__  . '/postcodes.json');
+            foreach (json_decode($postcodes, true) as $postcodeData) {
+                if ($postcodeData['postcode'] == $data['dest_postcode']) {
+                    $foundData = $postcodeData;
+                    break;
+                }
+            }
+
+            $data['final_address'] = [
+                'dest_street' => $data['dest_street'],
+                'dest_region_code' => $foundData ? $foundData['state'] : $data['dest_region_code'],
+                'dest_city' => $foundData ? $foundData['locality'] : $data['dest_city'],
+                'dest_postcode' => $foundData ? $foundData['postcode'] : $data['dest_postcode'],
+            ];
+
             $this->quoteCollectionFactory->create()->addFieldToFilter('cart_id', $cart->getId())->walk('delete');
             $response = $this->spojitClient->sendRequest($this->getScopeConfig('workflow_token'), $data);
 
-            if ($response && array_key_exists('quotes', $response) && is_array($response['quotes'])) {
-                $quotes = $response['quotes'];
+            if ($response &&
+                array_key_exists('GetDeliveryOptionsResult', $response) &&
+                array_key_exists('otheroptions', $response['GetDeliveryOptionsResult']) &&
+                array_key_exists('DeliveryOption', $response['GetDeliveryOptionsResult']['otheroptions']) &&
+                is_array($response['GetDeliveryOptionsResult']['otheroptions']['DeliveryOption'])) {
+                $quotes = $response['GetDeliveryOptionsResult']['otheroptions']['DeliveryOption'];
             } else {
                 return false;
             }
@@ -206,19 +259,27 @@ class SpojitShipping extends AbstractCarrier implements CarrierInterface
             foreach ($quotes as $quote) {
 
                 // if nothing has been mapped in the quote continue
-                if (!array_key_exists('name', $quote)) {
+                if (!array_key_exists('carriername', $quote)) {
                     continue;
                 }
 
-                $methodCode = $this->getMethodCode($quote['name'], $quote['service']);
+                $service = $quote['service'];
+
+                if (array_key_exists('serviceoption', $quote)) {
+                    if (array_key_exists('option', $quote['serviceoption']) && $quote['serviceoption']['option']) {
+                        $service = $service . ' ' . $quote['serviceoption']['option'];
+                    }
+                };
+
+                $methodCode = $this->getMethodCode($quote['carriername'], $service);
                 $carrier = $this->carrierCollectionFactory->create()->addFieldToFilter('code', $methodCode)->getFirstItem();
 
                 if (!$carrier->getId()) {
                     $carrier = $this->carrierFactory->create();
                     $carrier
                         ->setCode($methodCode)
-                        ->setName($quote['name'])
-                        ->setService($quote['service'])
+                        ->setName($quote['carriername'])
+                        ->setService($service)
                         ->save();
                 }
 
@@ -227,9 +288,9 @@ class SpojitShipping extends AbstractCarrier implements CarrierInterface
                     ->setCartId($cart->getId())
                     ->setCarrierId($carrier->getId())
                     ->setMethod($methodCode)
-                    ->setMethodTitle($quote['name'] . ' - ' .  $quote['service'])
-                    ->setPrice($quote['price'])
-                    ->setOptionId($quote['optionId'])
+                    ->setMethodTitle($quote['carriername'] . ' - ' .  $service)
+                    ->setPrice(str_replace(',', '', $quote['primarypricing']))
+                    ->setOptionId($quote['optionid'])
                     ->save();
             }
         }
